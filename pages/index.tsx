0824c393dadacb7f8aa600fe1fa552858e451c7e
ValRedstone <valdesign.dev@gmail.com>
2022-12-27 20:45:19 +0100

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,49 @@ export default function Home() {
           <h2>I&apos;m a 15 year old french web developer.</h2>
 
           <p>
-            I&apos;m currently working on a lot of projects, like this website, a discord bot, a minecraft server, and more.
+            I&apos;m currently working on a lot of projects, like this website, a minecraft mod, and more.
           </p>
+
+          <div className="skills-list">
+            <h3>Skills</h3>
+            <div>
+              <div className="skills">
+                <p>I know a lot of languages, like:</p>
+                <ul>
+                  <li>HTML</li>
+                  <li>CSS</li>
+                  <li>JavaScript</li>
+                  <li>TypeScript</li>
+                  <li>Python</li>
+                  <li>Java</li>
+                  <li>PHP</li>
+                  <li>SQL</li>
+                </ul>
+              </div>
+
+              <div className="skills">
+                <p>I also know a lot of frameworks, like:</p>
+                <ul>
+                  <li>React</li>
+                  <li>Next.js</li>
+                  <li>Laravel</li>
+                </ul>
+              </div>
+
+              <div className="skills">
+                <p>I also know a lot of tools, like:</p>
+                <ul>
+                  <li>Git</li>
+                  <li>GitHub</li>
+                  <li>VSCode</li>
+                  <li>IntelliJ</li>
+                  <li>Figma</li>
+                  <li>Adobe Photoshop</li>
+                  <li>Adobe Illustrator</li>
+                </ul>
+              </div>
+            </div>
+          </div>
         </section>
       </main>
     </>

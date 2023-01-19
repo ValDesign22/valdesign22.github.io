e5cc00009a886ab92fb6a39d92372487b6552498
ValRedstone <valdesign.dev@gmail.com>
2023-01-19 10:57:18 +0100

    update

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
         <section className="contact">
           <h1>Contact</h1>
 
-          <p>Vous pouvez me contacter par mail à l&apos;adresse suivante: <a href="mailto:valdesign.dev@gmail.com">valdesign.dev@gmail.com</a></p>
+          <p>Vous pouvez me contacter par mail à l&apos;adresse suivante: <a href="mailto:valdesign.dev@gmail.com" target="_blank" rel="noreferrer">valdesign.dev@gmail.com</a></p>
 
           <div className="form">
             <h2>Formulaire de contact</h2>
@@ -23,11 +23,11 @@ export default function Contact() {
             <form>
               <div className="up-part">
                 <div className="input">
-                  <label htmlFor="name">Nom</label>
+                  <label htmlFor="name">Nom<span className="required">*</span></label>
                   <input type="text" name="name" id="name" required />
                 </div>
                 <div className="input">
-                  <label htmlFor="email">Email</label>
+                  <label htmlFor="email">Email<span className="required">*</span></label>
                   <input type="email" name="email" id="email" required />
                 </div>
                 <div className="input">
@@ -37,14 +37,15 @@ export default function Contact() {
               </div>
               <div className="middle-part">
                 <div className="input">
-                  <label htmlFor="subject">Sujet</label>
+                  <label htmlFor="subject">Sujet<span className="required">*</span></label>
                   <input type="text" name="subject" id="subject" required />
                 </div>
               </div>
               <div className="down-part">
                 <div className="input">
-                  <label htmlFor="message">Message</label>
+                  <label htmlFor="message">Message<span className="required">*</span></label>
                   <textarea name="message" id="message" cols={30} rows={5} required />
+
                 </div>
               </div>
             </form>

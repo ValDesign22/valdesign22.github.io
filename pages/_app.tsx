107a89dfe043da935f4ba37eefe2290acb654c93
ValRedstone <valdesign.dev@gmail.com>
2023-01-31 19:54:00 +0100

    Add robots meta

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,6 +26,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
 
         <link rel="manifest" href="/site.webmanifest" />
+        <meta name="robots" content="index, follow" />
       </Head>
       <Component {...pageProps} />
       <Footer />

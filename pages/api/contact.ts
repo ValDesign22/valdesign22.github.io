49cb9211e8769daf212eed6252ffb9e48daf1f7b
ValDesign <valdesign.dev@gmail.com>
2023-04-24 14:09:24 +0200

    Update contact.ts

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -17,52 +17,52 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         pass: process.env.NODEMAILER_PASS
       }
     });
-  } catch(err) {
-    return res.json({error: err});
-  }
 
-  transporter.sendMail({
-    from: process.env.NODEMAILER_EMAIL,
-    to: email,
-    subject: "Demande de contact avec ValDesign",
-    html: `
-      <div style="
-        background: linear-gradient(45deg, rgb(7, 0, 121) 0%, rgb(29, 29, 197) 35%, rgba(0,106,255,1) 100%);
-        padding: 2rem;
-        border-radius: 1rem;
-        color: #fff;
-        font-family: 'Roboto', sans-serif;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-      ">
+    transporter.sendMail({
+      from: process.env.NODEMAILER_EMAIL,
+      to: email,
+      subject: "Demande de contact avec ValDesign",
+      html: `
         <div style="
-          background: rgba(0, 0, 0, 0.5);
+          background: linear-gradient(45deg, rgb(7, 0, 121) 0%, rgb(29, 29, 197) 35%, rgba(0,106,255,1) 100%);
           padding: 2rem;
           border-radius: 1rem;
-          text-align: center;
-          width: 100%;
+          color: #fff;
+          font-family: 'Roboto', sans-serif;
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
         ">
-          <h1 style="font-size: 1.5rem; font-weight: 600; margin-bottom: 1rem;">Demande de contact</h1>
-          <p style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem;">Bonjour ${name}, vous venez de faire une demande de contact avec ValDesign.</p>
-          <p style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem;">Voici ses coordonnées:</p>
-          <ul style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem; list-style: none; padding: 0; text-align: left; margin-left: 1rem; margin-top: 1rem; line-height: 1.5;">
-            <li>Email: <a href="mailto:${email}" style="color: rgb(230, 230, 230); text-decoration: none;">${email}</a></li>
-            ${phone ? `<li>Téléphone: ${phone}</li>` : ""}
-          </ul>
-          <h1 style="font-size: 1.5rem; font-weight: 600; margin-bottom: 1rem;">Sujet du message</h1>
-          <p style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem;">${subject}</p>
-          <h1 style="font-size: 1.5rem; font-weight: 600; margin-bottom: 1rem;">Message</h1>
-          <p style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem;">${message}</p>
+          <div style="
+            background: rgba(0, 0, 0, 0.5);
+            padding: 2rem;
+            border-radius: 1rem;
+            text-align: center;
+            width: 100%;
+          ">
+            <h1 style="font-size: 1.5rem; font-weight: 600; margin-bottom: 1rem;">Demande de contact</h1>
+            <p style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem;">Bonjour ${name}, vous venez de faire une demande de contact avec ValDesign.</p>
+            <p style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem;">Voici ses coordonnées:</p>
+            <ul style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem; list-style: none; padding: 0; text-align: left; margin-left: 1rem; margin-top: 1rem; line-height: 1.5;">
+              <li>Email: <a href="mailto:${email}" style="color: rgb(230, 230, 230); text-decoration: none;">${email}</a></li>
+              ${phone ? `<li>Téléphone: ${phone}</li>` : ""}
+            </ul>
+            <h1 style="font-size: 1.5rem; font-weight: 600; margin-bottom: 1rem;">Sujet du message</h1>
+            <p style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem;">${subject}</p>
+            <h1 style="font-size: 1.5rem; font-weight: 600; margin-bottom: 1rem;">Message</h1>
+            <p style="font-size: 1.2rem; font-weight: 400; margin-bottom: 1rem;">${message}</p>
+          </div>
         </div>
-      </div>
-    `
+      `
     }, (err, _info) => {
       if (err) return res.json({message: "Email failed to send",error: err});
       res.status(200).json({message: "Email sent successfully"});
     });
+  } catch(err) {
+    return res.json({error: err});
+  }
 
   res.status(200).json({message: "Email sent successfully"});
 }

1392c59954bde009b1303136fd9e6e8a25a9b660
ValDesign <valdesign.dev@gmail.com>
2023-08-12 19:04:59 +0200

    updated projects

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,34 +1,9 @@
-import Carousel from "../components/Carousel";
 import NavBar from "../components/NavBar";
-import ImageStyled from "../components/ImageStyled";
 import projects from "../public/projects.json";
-
-import {useState} from "react";
-import {AiFillGithub, AiOutlineLink} from "react-icons/ai";
-import {IoLogoSass, IoLogoPython} from "react-icons/io";
-import {RxCaretLeft, RxDotFilled, RxDot, RxCaretRight} from "react-icons/rx";
-import {SiTypescript, SiReact} from "react-icons/si";
-import {TbBrandNextjs} from "react-icons/tb";
+import Image from "next/image";
+import { AiFillGithub, AiOutlineLink } from "react-icons/ai";
 
 export default function Projects() {
-  const [currentImage, setCurrentImage] = useState(0);
-  const [currentProject, setCurrentProject] = useState(0);
-
-  const nextImage = (project: { name: string; carouselImages: { src: string; alt: string; }[]; }) => {
-    setCurrentProject(projects.findIndex((p) => p.name === project.name));
-    setCurrentImage((currentImage + 1) % project.carouselImages.length);
-  };
-  const prevImage = (project: { name: string; carouselImages: { src: string; alt: string; }[]; }) => {
-    setCurrentProject(projects.findIndex((p) => p.name === project.name));
-    setCurrentImage(
-      currentImage === 0 ? project.carouselImages.length - 1 : currentImage - 1
-    );
-  };
-  const toImage = (project: { name: string; }, index: number) => {
-    setCurrentProject(projects.findIndex((p) => p.name === project.name));
-    setCurrentImage(index);
-  }
-
   return (
     <>
       <main>
@@ -39,7 +14,7 @@ export default function Projects() {
           <div className="list">
             {projects.map((project, index) => (
               <div className="project" key={index}>
-                <Carousel projectIndex={index} key={index} />
+                {/* <Carousel projectIndex={index} key={index} />
                 <h2>{project.name}</h2>
                 <p>{project.description.split("\n").map((item, key) => <span key={key}>{item}<br /></span>)}</p>
                 <div className="links">
@@ -82,6 +57,33 @@ export default function Projects() {
                       </span>}
                       </>
                     ))}
+                  </div>
+                </div> */}
+                {project.image &&
+                  <Image
+                    src={project.image}
+                    alt={project.name} 
+                    width={500}
+                    height={500}
+                  />
+                }
+
+                <div className="textarea">
+                  <h2>{project.name}</h2>
+
+                  <p>{project.description.split("\n").map((item, key) => <span key={key}>{item}<br /></span>)}</p>
+                </div>
+
+                <div className="links">
+                  <h3>Liens</h3>
+
+                  <div className="inner">
+                    <a href={project.repo} target="_blank" rel="noopener noreferrer">
+                      <AiFillGithub />
+                    </a>
+                    <a href={project.url} target="_blank" rel="noopener noreferrer">
+                      <AiOutlineLink />
+                    </a>
                   </div>
                 </div>
               </div>
